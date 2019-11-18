8467225ea04333e29db6d680abd95c6625681368
MorganeSauneuf <morgane.sauneuf@gmail.com>
2019-11-18 16:23:36 +0100

    add json files and map on themeSection

--- src/javascript/sections/ThemeSection.js
+++ src/javascript/sections/ThemeSection.js
@@ -4,14 +4,16 @@ class ThemeSection extends React.Component {
     constructor(props) {
         super(props);
 
+        const pathToAssets = require.context(`../../assets/images/`, true);
+
         this.sectionStyle = {
-            backgroundImage: `url(${this.props.image})`
+            backgroundImage: `url(${pathToAssets(`./${this.props.image}`)})`
         }
     }
 
     render() {
         return (
-            <section className='section-theme'  style={this.sectionStyle}>
+            <section className='section-theme' style={this.sectionStyle}>
                 <h1 className="section-theme__title">
                     {this.props.title}
                 </h1>
@@ -19,7 +21,7 @@ class ThemeSection extends React.Component {
         )
     }
 
-
+    
 }
 
 export default ThemeSection;
