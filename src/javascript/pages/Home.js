07c694fc16bc721d7df066989181b2d3cc5aa67f
leochocolat <l.mouraire@outlook.fr>
2019-11-20 14:01:08 +0100

    a

--- src/javascript/pages/Home.js
+++ src/javascript/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ThemeSection from '../sections/ThemeSection';
+import ThemeSectionComponent from '../components/ThemeSectionComponent';
 import CursorComponent from '../components/CursorComponent';
 import SmoothScrollModule from '../modules/SmoothScrollModule';
 import ThemeContext from '../provider/ThemeContext';
@@ -18,7 +18,7 @@ export default class Home extends React.Component {
         {({username, themeList}) => (
           <section className="js-scroll-container">
             <div className="page-home__username" data-scroll data-scroll-sticky data-scroll-target=".js-scroll-container">Hello {username}</div>
-            {themeList.map((theme, index) => <ThemeSection key={index} {...theme} />)}
+            {themeList.map((theme, index) => <ThemeSectionComponent key={index} {...theme} />)}
             <CursorComponent />
           </section>
         )}
