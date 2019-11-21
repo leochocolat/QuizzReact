2d159951274fcfe48770d1ab16ac8410d52c980c
leochocolat <l.mouraire@outlook.fr>
2019-11-21 12:35:12 +0100

    timer

--- src/javascript/pages/Quizz.js
+++ src/javascript/pages/Quizz.js
@@ -1,6 +1,12 @@
 import React, {useEffect} from 'react';
+import { NavLink } from 'react-router-dom';
 import ThemeContext from '../provider/ThemeContext';
 import ScoreContext from '../provider/ScoreContext';
+
+import TimeModule from '../modules/TimerModule';
+
+let timeModule;
+const DURATIONMAX = 10;
 
 const Quizz = (props) => {
 
@@ -11,8 +17,8 @@ const Quizz = (props) => {
   const scoreContext = React.useContext(ScoreContext);
 
   const [questionId, setQuestionId] = React.useState(0);
+  let [duration, setDuration] = React.useState(0);
   const [points, setPoints] = React.useState([]);
-  // setPoints(points => []);
   const theme = context.getTheme(id);
 
   const style = {
@@ -21,51 +27,71 @@ const Quizz = (props) => {
   
   useEffect(() => {
     context.setTheme(id);
+    timeModule = new TimeModule(DURATIONMAX);
+    updateTime();
     return () => context.resetTheme();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id])
+  }, [id]);
 
-  function handleResponse(e) {
-    verifyResponse(e.target.innerHTML);
+  useEffect(() => {
+    if (duration === DURATIONMAX) {
+      verifyResponse('');
+      nextQuestion();
+    }
+  }, [duration])
 
-    if (questionId >= context.json.quizz.débutant.length - 1) {
-      console.log('fini');
+  function updateTime() {
+    setInterval(() => {
+      setDuration(duration => duration + 1);
+    }, 1000);
+  }
+
+  function nextQuestion() {
+    setDuration(0);
+    timeModule.restart();
+    if (context.json && questionId >= context.json.quizz.débutant.length - 1) {
       scoreContext.setScore(id, points);
     } else {
       setQuestionId(questionId => questionId + 1);
     };
   }
 
-  function verifyResponse(response) {
-      if (response === context.json.quizz.débutant[questionId].réponse) {
-        setPoints(points => { points.push(1); return points });
-        console.log('true');
-      } else {
-        setPoints(points => { points.push(0); return points });
-        console.log('false');
-      }
+  function handleResponse(e) {
+    verifyResponse(e.target.innerHTML);
+    nextQuestion();
   }
+
+  function verifyResponse(response) {
+    if (context.json && response === context.json.quizz.débutant[questionId].réponse) {
+      setPoints(points => { points.push(1); return points });
+    } else {
+      setPoints(points => { points.push(0); return points });
+    }
+  }
+
 
   return (
     <section className="page-quizz">
-    <header className="header-quizz" style={style}>
-      <h1 className="header-quizz__heading">Question {context.json && context.json.quizz.débutant[questionId].id}</h1>
-    </header>
-    <section className="section-question">
-      <h2 className="section-question__subheading">{theme.title}</h2>
-      <div className="section-question__question">
-        <p>{context.json && context.json.quizz.débutant[questionId].question}</p>
-      </div>
-      <ul className="section-question__list-response">
-        {context.json && context.json.quizz.débutant[questionId].propositions.map((response, index) => <li onClick={handleResponse} className="section-question__list-response-item" key={index}>{response}</li>)}  
-      </ul>
-      <div className="section-question__progress">
-        {context.json && context.json.quizz.débutant[questionId].id}/10
-      </div>
-      <button className="section-question__leave-button">Quitter</button>
+      <header className="header-quizz" style={style}>
+        <h1 className="header-quizz__heading">Question {context.json && context.json.quizz.débutant[questionId].id}</h1>
+      </header>
+      <section className="section-question">
+        <div className="section-question__timer js-timer">{duration}</div>
+        <h2 className="section-question__subheading">{theme.title}</h2>
+        <div className="section-question__question">
+          <p>{context.json && context.json.quizz.débutant[questionId].question}</p>
+        </div>
+        <ul className="section-question__list-response">
+          {context.json && context.json.quizz.débutant[questionId].propositions.map((response, index) => <li onClick={handleResponse} className="section-question__list-response-item" key={index}>{response}</li>)}  
+        </ul>
+        <div className="section-question__progress">
+          {context.json && context.json.quizz.débutant[questionId].id}/10
+        </div>
+        
+        <NavLink to='/' className="section-question__leave-button">Quitter</NavLink>
+      </section>
     </section>
-  </section>
   )
 
 }
+
 export default Quizz;
